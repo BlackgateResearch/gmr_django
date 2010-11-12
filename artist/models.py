60e38d759299f3576c2bf6310a501d179eb93f60
Tristram Oaten <tristram@oaten.name>
2010-11-12 22:32:31 +0000

    tweaked filefield to imagefield, as it's an image!

--- artist/models.py
+++ artist/models.py
@@ -4,4 +4,4 @@ class Artist(models.Model):
     user = models.ForeignKey('auth.User')
     name = models.CharField(max_length = 50)
     description = models.TextField()
-    picture = models.FileField(upload_to="artist/pictures")
+    picture = models.ImageField(upload_to="artist/pictures")
