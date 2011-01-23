afb25a160bf298b1854cab636d54dffa7353f834
Karl Williams <karl@deadlight.net>
2011-01-23 22:14:43 +0000

    Started to code basic skeletons for gmrPlaylist.js and gmrPlayer.js (not functioning)

---
+++ media/js/gmrPlaylist.js
@@ -0,0 +1,29 @@
+/*
+ * gmrPlaylist
+ * Loads and manipulates a playlist of tracks
+ * namespace: gmr.playlist
+ * dependencies: jquery 1.4.4, gmrPlayer
+ */
+  
+gmr.playlist = function() {
+  //private
+    
+ 
+  return {
+    //public
+    var tracks;
+    
+    linkToPlayer() {
+      //TODO : set gmr.player.onended to something
+    }    
+              
+    load() {
+      
+    }
+    
+    save() {
+      
+    }
+      
+  }
+}
