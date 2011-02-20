fcb0104f828233c55ac488c8f35fdf0931a5876b
oatman <oatman@oatman-pc.(none)>
2011-02-20 16:17:32 +0000

    Fixed a bad import, tidied urls

--- track/urls.py
+++ track/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls.defaults import *
-from track.views import track
 
 urlpatterns = patterns('',
     (r'^(?P<track_id>\d+)/$', 'track.views.track_get'),
--- urls.py
+++ urls.py
@@ -1,29 +1,18 @@
 from django.conf.urls.defaults import *
 import settings
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin_tools/', include('admin_tools.urls')),
-    # Example:
-    # (r'^gmr_django/', include('gmr_django.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    #(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
-    
     
     (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
     {'document_root': settings.MEDIA_ROOT}),
 
     (r'^radio/', include('radio.urls')),
     (r'^track/', include('track.urls')),
-    
     (r'^playlist/(?P<playlist_id>\d+)/$', 'track.views.playlist_get'),
 )
 
