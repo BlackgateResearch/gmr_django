f77b8e52c21fa2ea49adc60f695737d3f9a97bbd
Tristram Oaten <tristram@oaten.name>
2011-03-09 21:05:31 +0000

    commented track models and removed PlayToEnd, now Play tracks the duration of the play.

--- track/admin.py
+++ track/admin.py
@@ -1,6 +1,6 @@
 from track.models import Track, Preset, Track_pass, Track_vote
 from track.models import Playlist, PlaylistTrack 
-from track.models import Play, PlayToEnd
+from track.models import Play
 from django.contrib import admin
 
 class TrackAdmin(admin.ModelAdmin):
@@ -15,8 +15,5 @@ admin.site.register(Preset)
 admin.site.register(Playlist)
 admin.site.register(PlaylistTrack)
 admin.site.register(Play)
-admin.site.register(PlayToEnd)
 admin.site.register(Track_pass)
 admin.site.register(Track_vote)
-
-
--- track/models.py
+++ track/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Track(models.Model):
+    '''
+    Stores track information
+    '''
     artist =      models.ForeignKey('artist.Artist')
     name =        models.CharField(max_length = 50)
     description = models.TextField()
@@ -13,12 +16,23 @@ class Track(models.Model):
 
 
 class Track_vote(models.Model):
+    '''
+    Ensures a user can only vote on a track once.
+    TODO: wipe this once a week? Maybe use a bool
+    '''
     track =     models.ForeignKey('track.Track')
     user =      models.ForeignKey('auth.User')
     vote_time = models.TimeField()
     
 
 class Track_pass(models.Model):
+    '''
+    TODO: Does oli-charlesworth's solution work?
+    http://stackoverflow.com/questions/4642524/maths-approximating-the-mean-without-storing-the-whole-data-set
+    How about storing the cumulative vote count, and the number of votes?
+    (i.e. total=total+vote, numVotes=numVotes+1).
+    That way, you can get the exact mean by dividing one by the other.
+    '''
     track =      models.ForeignKey('track.Track')
     positivity = models.IntegerField()
     aggression = models.IntegerField()
@@ -31,6 +45,9 @@ class Track_pass(models.Model):
 
 
 class Preset(models.Model):
+    '''
+    A user's PASS preset, named.
+    '''
     name =       models.CharField(max_length = 50)
     positivity = models.IntegerField()
     aggression = models.IntegerField()
@@ -42,6 +59,9 @@ class Preset(models.Model):
         return unicode(self.name)
 
 class Playlist(models.Model):
+    '''
+    A named track container, linking table, PlaylistTrack.
+    '''
     name =   models.CharField(max_length = 50)
     user =   models.ForeignKey('auth.User')
     tracks = models.ManyToManyField('Track', through='PlaylistTrack')
@@ -51,6 +71,10 @@ class Playlist(models.Model):
         
         
 class PlaylistTrack(models.Model):
+    '''
+    A linking table between Playlist and Track, identifying if a Track
+    is in a Playlist
+    '''
     playlist = models.ForeignKey('track.Playlist')
     track =    models.ForeignKey('track.Track')
     position = models.IntegerField()
@@ -63,12 +87,20 @@ class PlaylistTrack(models.Model):
     
     
 class Play(models.Model):
+    '''
+    A log of when a user played a track, and how long they listened
+    to it for.
+    '''
     user =        models.ForeignKey('auth.User')
     track =       models.ForeignKey('track.Track')
     played_time = models.DateTimeField()
+    duration =    models.IntegerField()
     
-    
+
+'''
+#integrated into Play
 class PlayToEnd(models.Model):
     user =          models.ForeignKey('auth.User')
     track =         models.ForeignKey('track.Track')
     finished_time = models.DateTimeField()
+'''
