108b06fe11671ae35602c47566209f49d3daf20e
Karl Williams <karl@deadlight.net>
2011-04-23 15:58:48 +0100

    Fixed previous and next and implemented playlist items as links

--- media/js/gmrRadio.js
+++ media/js/gmrRadio.js
@@ -19,6 +19,7 @@ gmr.radio = function() {
       $.ajax({
         url: "/playlist/1",
         context: document.body,
+        contentType: 'application/json',
         success: function(response){
           gmr.radio.playlist = response;
           console.log(gmr.radio.playlist);
@@ -33,18 +34,25 @@ gmr.radio = function() {
     },
     
     savePlaylist : function() {
-      
+      $.ajax({
+        type: 'POST',
+        url: '',
+        data: gmr.radio.playlist,
+        success: function() {}
+      })
     },
 
     next : function() {
       if (gmr.radio.currentTrack < (gmr.radio.playlist.tracks.length - 1)) {
         console.log(gmr.radio.currentTrack);
         gmr.radio.currentTrack++;
+        gmr.radio.player.stop();
         $(gmr.radio.player.audio).empty();
         console.log(gmr.radio.currentTrack + ' : ' + gmr.radio.playlist.tracks[gmr.radio.currentTrack]);
         gmr.radio.player.addSource('http://127.0.0.1:8000/site_media/' +gmr.radio.playlist.tracks[gmr.radio.currentTrack].ogg);
         gmr.radio.player.addSource('http://127.0.0.1:8000/site_media/' +gmr.radio.playlist.tracks[gmr.radio.currentTrack].mp3);
         gmr.radio.player.play();
+        gmr.radio.updatePlaylist();
       } else {
         //end of playlist
         console.log('No more tracks');
@@ -52,15 +60,45 @@ gmr.radio = function() {
     },
 
     previous : function() {
-      
-    },
-
-    pause : function() {
-      
+      if (gmr.radio.currentTrack > 0) {
+        console.log(gmr.radio.currentTrack);
+        gmr.radio.currentTrack--;
+        gmr.radio.player.stop();
+        $(gmr.radio.player.audio).empty();
+        console.log(gmr.radio.currentTrack + ' : ' + gmr.radio.playlist.tracks[gmr.radio.currentTrack]);
+        gmr.radio.player.addSource('http://127.0.0.1:8000/site_media/' +gmr.radio.playlist.tracks[gmr.radio.currentTrack].ogg);
+        gmr.radio.player.addSource('http://127.0.0.1:8000/site_media/' +gmr.radio.playlist.tracks[gmr.radio.currentTrack].mp3);
+        gmr.radio.player.play();
+        gmr.radio.updatePlaylist();
+      } else {
+        //end of playlist
+        console.log('At first track');
+      }
     },
 
     assignPlayerCallback : function() {
       gmr.radio.player.assignEndCallback(gmr.radio.next);
+    },
+
+    updatePlaylist : function() {
+      $('#playlist').empty();
+      for (var i in gmr.radio.playlist.tracks) {
+        var liExtra = '';
+        if (gmr.radio.currentTrack == i) { liExtra = ' style="background-color: red;"' }
+        $('#playlist').append('<li' + liExtra + '><a onclick="gmr.radio.jumpTo(' + i + ');">' + gmr.radio.playlist.tracks[i].name + '</a></li>');
+      }
+    },
+
+    jumpTo : function(track) {
+      if (track < (gmr.radio.playlist.tracks.length)){
+        gmr.radio.currentTrack = track;
+        gmr.radio.updatePlaylist();
+        gmr.radio.player.stop();
+        $(gmr.radio.player.audio).empty();
+gmr.radio.player.addSource('http://127.0.0.1:8000/site_media/' +gmr.radio.playlist.tracks[gmr.radio.currentTrack].ogg);
+        gmr.radio.player.addSource('http://127.0.0.1:8000/site_media/' +gmr.radio.playlist.tracks[gmr.radio.currentTrack].mp3);
+        gmr.radio.player.play();
+      } 
     }
   }
 }();
